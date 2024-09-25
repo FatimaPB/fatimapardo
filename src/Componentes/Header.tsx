f83da029483f415d56c029b3516ea2d97c3acb18
GABBRIELJUA <20221040@uthh.edu.mx>
2024-09-25 02:59:10 -0600

    lkjfvycdsy

--- src/Componentes/Header.tsx
+++ src/Componentes/Header.tsx
@@ -9,6 +9,12 @@ const HeaderCifrado = () => {
         setIsOpen(!isOpen);
     };
 
+    const closeMenu = () => {
+        if (window.innerWidth <= 768) { // Ajusta el ancho máximo según tus necesidades
+            setIsOpen(false);
+        }
+    };
+
     return (
         <header className="header">
             <nav className="navbar">
@@ -18,13 +24,13 @@ const HeaderCifrado = () => {
                 </button>
                 <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
                     <li className="nav-item">
-                        <Link to="/guia">Guía</Link> {/* Reemplazar <a> por <Link> */}
+                        <Link to="/guia" onClick={closeMenu}>Guía</Link> {/* Cerrar menú después de clic en Link */}
                     </li>
                     <li className="nav-item">
-                        <Link to="/cesar">Cifrado César</Link> {/* Reemplazar <a> por <Link> */}
+                        <Link to="/cesar" onClick={closeMenu}>Cifrado César</Link> {/* Cerrar menú después de clic en Link */}
                     </li>
                     <li className="nav-item">
-                        <Link to="/escitala">Cifrado Escítala</Link> {/* Reemplazar <a> por <Link> */}
+                        <Link to="/escitala" onClick={closeMenu}>Cifrado Escítala</Link> {/* Cerrar menú después de clic en Link */}
                     </li>
                 </ul>
             </nav>
